2b6ebf06aea3a8b2d0205c050385c0d0dfc09b0a
mobilacks <198098276+mobilacks@users.noreply.github.com>
2025-11-28 12:50:41 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+// app/page.tsx
+
 import Link from 'next/link';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
-import { authOptions } from './api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 
 export default async function HomePage() {
   // If user is already logged in, redirect to dashboard
